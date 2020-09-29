cb3236844726e86d6b581441a99eacde21b3ebd6
Svetlin <svetlimladenov@gmail.com>
2020-09-29 10:19:03 +0300

    Add getters and setters for Prog

---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,12 @@
+// ConsoleApplication2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "Prog.cpp"
+
+using namespace std;
+
+int main()
+{
+  
+}
---
+++ ConsoleApplication2/ConsoleApplication2/Prog.cpp
@@ -0,0 +1,31 @@
+class Prog {
+private:
+	int n;
+	int x;
+	int inc;
+
+public:
+	void setN(int value) {
+		n = value;
+	}
+
+	int getN() {
+		return n;
+	}
+
+	void setX(int value) {
+		x = value;
+	}
+
+	int getX() {
+		return x;
+	}
+
+	void setInc(int value) {
+		inc = value;
+	}
+
+	int getInc() {
+		return inc;
+	}
+};
